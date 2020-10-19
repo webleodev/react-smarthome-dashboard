a68a98c94e62ab290df63ade55a5b5731cf62da4
ITDev-front <web.leo.develop@gmail.com>
2020-10-19 14:23:53 +0300

    Updated mainPage, added and updated styles

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,12 +4,18 @@ import welcomeImage from '../assets/images/welcome.png';
 import weatherIcon from '../assets/svg/weather.svg';
 import termometerIcon from '../assets/svg/termometer.svg';
 import aDropIcon from '../assets/svg/adrop.svg';
+import refridgeratorIcon from '../assets/svg/refrigerator.svg';
 
 function MainPage() {
     const [visiblePlaceList, setVisiblePlaceList] = React.useState(false);
+    const [checkedCheckbox, setCheckedCheckbox] = React.useState(false);
 
     const toggleVisible = () => {
         setVisiblePlaceList(!visiblePlaceList);
+    }
+
+    const toggleCheckbox = () => {
+        setCheckedCheckbox(!checkedCheckbox);
     }
 
     return (
@@ -38,35 +44,63 @@ function MainPage() {
                 <img className="welcome__picture" id="welcome__picture" src={welcomeImage} alt="welcome pic" />
             </div>
 
-            <div className="indicators__block" id="devices__block">
-                <div className="information__block" id="humidity">
-                    <img src={aDropIcon} alt="humidity icon" className="information__img" id="humidity__icon" />
+            <div className="temperature__block">
+                <div className="temperature__header">
+                    <img src={termometerIcon} alt="termometer icon" className="termometer__icon" />
 
-                    <span className="information__value" id="humidity__value">
-                        35%
-                    </span>
+                    <div className={`__select ${visiblePlaceList === true ? 'active' : ''}`} id="place__select" onClick={toggleVisible}>
+                        <span className="__value" id="place__value">
+                            Bedroom
+                        </span>
+
+                        {visiblePlaceList && (
+                            <ul className="__list" id="place__list">
+                                <li className="__item">Living Room</li>
+                                <li className="__item">Kitchen</li>
+                                <li className="__item">Bedroom</li>
+                            </ul>
+                        )}
+                    </div>
+
+                    <div className="counter" id="temperature__counter">
+                        <span className="change__value" id="decrement__value">
+                            -
+                        </span>
+
+                        <span className="__value" id="temperature__value">
+                            +20&#8451;
+                        </span>
+
+                        <span className="change__value" id="increment__value">
+                            +
+                        </span>
+                    </div>
                 </div>
 
-                <div className="information__block" id="temperature">
-                    <img src={termometerIcon} alt="temperature icon" className="information__img" id="termometer__icon" />
-
-                    <span className="information__value" id="temperature__value">
-                        +20&#8451;
-                    </span>
+                <div className="temperature__body">
+                    <div className="room" id="room--bedroom">
+                        <div className="temperature__background"></div>
+                    </div>
                 </div>
 
-                <div className={`place__select ${visiblePlaceList === true ? 'active' : ''}`} id="place__select" onClick={toggleVisible}>
-                    <span className="place__value" id="place__value">
-                        Bedroom
-                    </span>
+                <div className="devices__block">
+                    <div className={`device__item ${checkedCheckbox === true ? 'switched' : ''}`}>
+                        <div className="device__top">
+                            <h1 className="switch__value">{`${checkedCheckbox === true ? 'on' : 'off'}`}</h1>
 
-                    {visiblePlaceList && (
-                        <ul className="place__list" id="place__list">
-                            <li className="place__item">Living Room</li>
-                            <li className="place__item">Kitchen</li>
-                            <li className="place__item">Bedroom</li>
-                        </ul>
-                    )}
+                            <input className="switch__checkbox" type="checkbox" name="switch__checkbox" id="checkbox-refridgerator" onChange={toggleCheckbox} />
+
+                            <label htmlFor="checkbox-refridgerator">
+                                <div className={`switch ${checkedCheckbox === true ? 'switched' : ''}`}>
+                                    <div className="dot"></div>
+                                </div>
+                            </label>
+                        </div>
+
+                        <img src={refridgeratorIcon} alt="refrigerator icon" className="device__icon" />
+
+                        <div className="device__name">Refridgerator</div>
+                    </div>
                 </div>
             </div>
         </section>
